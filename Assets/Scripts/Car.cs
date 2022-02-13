9b9edcd17c29bf38659d2110a6adf07b0c970cd8
Gene Collis <genecollis12@gmail.com>
2022-02-13 12:51:45 +1100

    Added Unity Project

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] private float engineForce = 100.0f;
+    [SerializeField] private float brakeForce = 100.0f;
+    [SerializeField] private float maxSteerAngle = 20.0f;
+
+    [SerializeField] private Transform centreOfMass;
+
+    [SerializeField][ReadOnly] private float motorTorque = 0;
+    [SerializeField][ReadOnly] private float brakeTorque = 0;
+    [SerializeField][ReadOnly] private float desiredDirection = 0;
+    [SerializeField][ReadOnly] private float currentDirection = 0;
+    [SerializeField][ReadOnly] private float combinedDriection = 0;
+    [SerializeField][ReadOnly] private float velocity = 0;
+
+    private new Rigidbody rigidbody;
+    private WheelColliderCollection wheelColliders;
+
+    private bool initialised = false;
+
+    private void OnEnable()
+    {
+        if (!initialised)
+        {
+            Init();
+        }
+    }
+
+    private void Init()
+    {
+        initialised = true;
+
+        rigidbody = GetComponent<Rigidbody>();
+        wheelColliders = GetComponentInChildren<WheelColliderCollection>();
+
+        Assert.IsNotNull(centreOfMass, "Missing centreOfMass transform reference.");
+        if (centreOfMass)
+        {
+            rigidbody.centerOfMass = centreOfMass.localPosition;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 localVelocity = transform.InverseTransformDirection(rigidbody.velocity);
+        velocity = localVelocity.z;
+
+        desiredDirection = verticalInput != 0 ? verticalInput / verticalInput : 0;
+        if (verticalInput < 0)
+        {
+            desiredDirection *= -1;
+        }
+        currentDirection = velocity != 0 ? velocity / velocity : 0;
+        if (Mathf.Abs(velocity) <= 0.1f)
+        {
+            currentDirection = 0;
+        }
+        else if (velocity < 0)
+        {
+            currentDirection *= -1;
+        }
+
+        combinedDriection = desiredDirection + currentDirection;
+
+        motorTorque = 0;
+        brakeTorque = 0;
+
+        if (desiredDirection != 0 && combinedDriection == 0)
+        {
+            brakeTorque = brakeForce;
+        }
+        else
+        {
+            motorTorque = verticalInput * engineForce;
+        }
+
+        wheelColliders.SetMotorTorque(motorTorque);
+        wheelColliders.SetBrakeTorque(brakeTorque);
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        wheelColliders.SetSteerAngle(horizontalInput * maxSteerAngle);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 pos = transform.position;
+            pos += Vector3.up * 1.0f;
+            transform.position = pos;
+
+            transform.rotation = Quaternion.AngleAxis(0.0f, transform.forward);
+        }
+    }
+}
---
+++ Assets/Scripts/WheelColliderCollection.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelColliderCollection : MonoBehaviour
+{
+    [SerializeField] private WheelCollider frontLeftWheelCollider;
+    [SerializeField] private WheelCollider frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider;
+    [SerializeField] private WheelCollider rearRightWheelCollider;
+
+    [SerializeField] private Transform frontLeftWheelTransform;
+    [SerializeField] private Transform frontRightWheelTransform;
+    [SerializeField] private Transform rearLeftWheelTransform;
+    [SerializeField] private Transform rearRightWheelTransform;
+
+    private bool initialised = false;
+
+    private void OnEnable()
+    {
+        if (!initialised)
+        {
+            Init();
+        }
+    }
+
+    private void Init()
+    {
+        initialised = true;
+    }
+
+    public void SetMotorTorque(float torque)
+    {
+        // All wheel drive?
+        frontLeftWheelCollider.motorTorque = torque;
+        frontRightWheelCollider.motorTorque = torque;
+        rearLeftWheelCollider.motorTorque = torque;
+        rearRightWheelCollider.motorTorque = torque;
+    }
+
+    public void SetBrakeTorque(float torque)
+    {
+        frontLeftWheelCollider.brakeTorque = torque;
+        frontRightWheelCollider.brakeTorque = torque;
+        rearLeftWheelCollider.brakeTorque = torque;
+        rearRightWheelCollider.brakeTorque = torque;
+    }
+
+    public void SetSteerAngle(float angle)
+    {
+        frontLeftWheelCollider.steerAngle = angle;
+        frontRightWheelCollider.steerAngle = angle;
+    }
+
+    private void Update()
+    {
+        UpdateWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+        UpdateWheel(rearRightWheelCollider, rearRightWheelTransform);
+    }
+
+    private void UpdateWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.position = pos;
+        wheelTransform.rotation = rot;
+    }
+
+
+}
